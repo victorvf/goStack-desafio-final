09cf782f65b1efb4c36e0eb8a333b32bb3d29a39
Victor Fontenele <victorvitoria11@hotmail.com>
2020-02-22 01:38:46 -0300

    separated middlewares and added new routes

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -3,44 +3,26 @@ import {promisify} from 'util';
 
 import authConfig from '../../config/auth';
 
-import User from '../models/User';
+export default async (request, response, next)=>{
+    const authHeader = request.headers.authorization;
 
-export default {
-    async authenticated(request, response, next){
-        const authHeader = request.headers.authorization;
+    if(!authHeader){
+        return response.status(401).json({
+            error: "token not provided"
+        });
+    };
 
-        if(!authHeader){
-            return response.status(401).json({
-                error: "token not provided"
-            });
-        };
+    const [, token] = authHeader.split(' ');
 
-        const [, token] = authHeader.split(' ');
+    try{
+        const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-        try{
-            const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
-            request.userId = decoded.id;
-
-            return next();
-        } catch(error){
-            return response.status(401).json({
-                error: "token invalid"
-            });
-        };
-
-
-    },
-
-    async isAdmin(request, response, next){
-        const user = await User.findByPk(request.userId);
-
-        if(!user.admin){
-            return response.status(401).json({
-                error: "user has not permission"
-            });
-        };
+        request.userId = decoded.id;
 
         return next();
-    }
+    } catch(error){
+        return response.status(401).json({
+            error: "token invalid"
+        });
+    };
 };
--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,12 @@ import FileController from './app/controllers/FileController.js';
 import DeliverymanController from './app/controllers/DeliverymanController.js';
 import OrderController from './app/controllers/OrderController.js';
 import NotificationController from './app/controllers/NotificationController.js';
-import DeliveredOrderController from './app/controllers/DeliveredOrderController.js';
+import CompletedDeliveriesController from './app/controllers/CompletedDeliveriesController.js';
+import DeliveriesController from './app/controllers/DeliveriesController.js';
+import CloseDeliveryController from './app/controllers/CloseDeliveryController.js';
 
 import authMiddleware from './app/middlewares/auth';
+import adminMiddleware from './app/middlewares/admin';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,13 +25,20 @@ routes.post('/session', SessionController.store);
 routes.get('/notifications/:id', NotificationController.index);
 routes.put('/notifications/:id/update', NotificationController.update);
 
-routes.get('/deliveryman/:id/orders-delivered', DeliveredOrderController.index);
+routes.get('/deliveryman/:id/deliveries', DeliveriesController.index);
+routes.put('/deliveryman/:id/deliveries', DeliveriesController.update);
+
+routes.post('/file/create', upload.single('file'), FileController.store);
+
+routes.put('/deliveryman/:id/close-delivery', CloseDeliveryController.update);
+
+routes.get('/deliveryman/:id/orders-delivered', CompletedDeliveriesController.index);
 
 // authentication
-routes.use(authMiddleware.authenticated);
+routes.use(authMiddleware);
 
 // verify user is admin
-routes.use(authMiddleware.isAdmin);
+routes.use(adminMiddleware);
 
 routes.get('/users', UserController.index);
 routes.get('/user/:id', UserController.show);
@@ -41,8 +51,6 @@ routes.get('/recipient/:id', RecipientController.show);
 routes.post('/recipient/create', RecipientController.store);
 routes.put('/recipient/:id/update', RecipientController.update);
 routes.delete('/recipient/:id/delete', RecipientController.delete);
-
-routes.post('/file/create', upload.single('file'), FileController.store);
 
 routes.get('/deliverymen', DeliverymanController.index);
 routes.get('/deliveryman/:id', DeliverymanController.show);
